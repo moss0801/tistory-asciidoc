0fa079bffab4a727bf95f102b766b7c0d11d82ab
reviewtechinterview <review.techinterview@gmail.com>
2019-04-09 01:48:34 +0900

    Initial commit

---
+++ src/main/java/com/moss/tistory/tistoryasciidoc/TistoryAsciidocApplication.java
@@ -0,0 +1,13 @@
+package com.moss.tistory.tistoryasciidoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TistoryAsciidocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TistoryAsciidocApplication.class, args);
+    }
+
+}
